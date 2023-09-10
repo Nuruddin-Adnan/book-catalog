718cf29d20f905891da23925ca0bb01b6dba828f
Nuruddin-Adnan <contact.codepixel@gmail.com>
2023-09-10 18:16:19 +0600

    home page design done

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,4 +1,11 @@
+import { useEffect, useState } from "react";
+import { Rating } from "react-simple-star-rating";
+
 export default function BookCard() {
+  const [rating, setRating] = useState(0);
+  useEffect(() => {
+    setRating(() => 0);
+  }, []);
   return (
     <>
       <div className="bg-white rounded-xl overflow-hidden shadow-lg hover:shadow-xl transition duration-300 transform">
@@ -6,14 +13,17 @@ export default function BookCard() {
           <img
             src="http://exprostudio.com/html/book_library/images/books/img-04.jpg"
             alt="Blog"
-            className="w-full object-cover transition-all hover:scale-105"
+            className="w-full h-52 object-cover transition-all hover:scale-105"
           />
         </div>
-        <div className="px-6 py-4">
+        <div className="px-6 py-2">
           <div className="text-xs text-gray-500">Sports</div>
           <div className="font-semibold text-md mb-2">Very good image</div>
           <div className="text-sm text-gray-700">By: Adnan</div>
           <div className="text-sm text-gray-600">10 september 2023</div>
+        </div>
+        <div className="star-rating px-6">
+          <Rating initialValue={rating} readonly size={20} />
         </div>
         <div className="px-6 pt-4 pb-2 mb-6">
           <a
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <section className="pb-20 pt-10">
+      <section className="pb-32 pt-5">
         <div className="container">
           <div className="text-center mb-10">
             <h2 className="text-2xl font-bold md:text-3xl md:leading-tight dark:text-white">
@@ -17,10 +17,39 @@ export default function Home() {
               Get all the latest Book here.
             </p>
           </div>
-          <div className="grid gap-5 grid-cols-5 lg:px-10 px-4">
+          <div className="grid gap-8 grid-cols-5 lg:px-10 px-4">
             <BookCard />
             <BookCard />
             <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
+            <BookCard />
+          </div>
+          <div className="text-center mt-10">
+            <a
+              className="inline-flex justify-center items-center gap-x-3 text-center bg-blue-600 hover:bg-blue-700 border border-transparent text-sm lg:text-base text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:focus:ring-offset-gray-800"
+              href="#"
+            >
+              Get All Books
+              <svg
+                className="w-2.5 h-2.5"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+              >
+                <path
+                  d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                ></path>
+              </svg>
+            </a>
           </div>
         </div>
       </section>
